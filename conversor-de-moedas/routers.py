cc55bd709c097472cf7c2b3c74268006b69d639f
grbalmeida <g.r.almeida@live.com>
2025-03-29 14:54:06 -0300

    Query parameter

--- conversor-de-moedas/routers.py
+++ conversor-de-moedas/routers.py
@@ -3,5 +3,5 @@ from fastapi import APIRouter
 router = APIRouter()
 
 @router.get('/converter/{from_currency}')
-def converter(from_currency: str):
+def converter(from_currency: str, to_currencies: str, price: float):
     return "It works"
