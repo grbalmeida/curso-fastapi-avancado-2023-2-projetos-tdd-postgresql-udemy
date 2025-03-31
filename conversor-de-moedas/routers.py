25610e9b528c7a353592e0ec745f5db199d7f53e
grbalmeida <g.r.almeida@live.com>
2025-03-31 14:30:01 -0300

    Validação de query parameter

--- conversor-de-moedas/routers.py
+++ conversor-de-moedas/routers.py
@@ -1,5 +1,5 @@
 from asyncio import gather
-from fastapi import APIRouter, Path
+from fastapi import APIRouter, Path, Query
 from converter import sync_converter, async_converter
 
 router = APIRouter(prefix='/converter')
@@ -7,8 +7,8 @@ router = APIRouter(prefix='/converter')
 @router.get('/{from_currency}')
 def converter(
     from_currency: str = Path(max_length=3, regex='^[A-Z]{3}$'),
-    to_currencies: str,
-    price: float
+    to_currencies: str = Query(max_length=50, regex='^[A-Z]{3}(,[A-Z]{3})*$'),
+    price: float = Query(gt=0)
 ):
     to_currencies = to_currencies.split(',')
 
@@ -26,7 +26,11 @@ def converter(
     return result
 
 @router.get('/async/{from_currency}')
-async def async_converter_router(from_currency: str, to_currencies: str, price: float):
+async def async_converter_router(
+    from_currency: str = Path(max_length=3, regex='^[A-Z]{3}$'),
+    to_currencies: str = Query(max_length=50, regex='^[A-Z]{3}(,[A-Z]{3})*$'),
+    price: float = Query(gt=0)
+):
     to_currencies = to_currencies.split(',')
 
     coroutines = []
