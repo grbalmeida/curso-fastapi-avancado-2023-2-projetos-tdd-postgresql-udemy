eb6e2359a62a320367a3c6d4aa45f6a7e87b605b
grbalmeida <g.r.almeida@live.com>
2025-04-05 00:48:13 -0300

    Caso de uso - listar categoria

--- produtos/app/test/conftest.py
+++ produtos/app/test/conftest.py
@@ -1,5 +1,6 @@
 import pytest
 from app.db.connection import Session
+from app.db.models import Category as CategoryModel
 
 @pytest.fixture()
 def db_session():
@@ -8,3 +9,25 @@ def db_session():
         yield session
     finally:
         session.close()
+
+@pytest.fixture()
+def categories_on_db(db_session):
+    categories = [
+        CategoryModel(name='Roupa', slug='roupa'),
+        CategoryModel(name='Carro', slug='carro'),
+        CategoryModel(name='Itens de cozinha', slug='itens-de-cozinha'),
+        CategoryModel(name='Decoração', slug='decoracao')
+    ]
+
+    for category in categories:
+        db_session.add(category)
+    db_session.commit()
+
+    for category in categories:
+        db_session.refresh(category)
+
+    yield categories
+
+    for category in categories:
+        db_session.delete(category)
+    db_session.commit()
