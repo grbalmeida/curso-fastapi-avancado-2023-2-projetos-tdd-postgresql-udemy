8636e65909921bb5d6b12c4f40baaff277820434
grbalmeida <g.r.almeida@live.com>
2025-04-07 15:20:37 -0300

    Caso de uso - atualizar produto

--- produtos/app/test/conftest.py
+++ produtos/app/test/conftest.py
@@ -1,6 +1,7 @@
 import pytest
 from app.db.connection import Session
 from app.db.models import Category as CategoryModel
+from app.db.models import Product as ProductModel
 
 @pytest.fixture()
 def db_session():
@@ -33,3 +34,26 @@ def categories_on_db(db_session):
         for category in categories:
             db_session.delete(category)
         db_session.commit()
+
+@pytest.fixture()
+def product_on_db(db_session):
+    category = CategoryModel(name='Camisas', slug='camisas')
+    db_session.add(category)
+    db_session.commit()
+
+    product = ProductModel(
+        name='Camisa Abibas',
+        slug='camisa-abibas',
+        price=100.99,
+        stock=20,
+        category_id=category.id
+    )
+
+    db_session.add(product)
+    db_session.commit()
+
+    yield product
+
+    db_session.delete(product)
+    db_session.delete(category)
+    db_session.commit()
