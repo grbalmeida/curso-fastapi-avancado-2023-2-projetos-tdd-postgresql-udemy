2378fa9c615e2b8d48c3977c07aa523cc17b42a2
grbalmeida <g.r.almeida@live.com>
2025-04-07 10:50:47 -0300

    Criando schema Product

---
+++ produtos/app/test/schemas/test_product_schema.py
@@ -0,0 +1,43 @@
+import pytest
+from app.schemas.product import Product
+
+def test_product_schema():
+    product = Product(
+        name='Camisa Mike',
+        slug='camisa-mike',
+        price=22.99,
+        stock=22
+    )
+
+    assert product.model_dump() == {
+        'name': 'Camisa Mike',
+        'slug': 'camisa-mike',
+        'price': 22.99,
+        'stock': 22
+    }
+
+def test_product_schema_invalid_slug():
+    with pytest.raises(ValueError):
+        product = Product(
+            name='Camisa Mike',
+            slug='camisa mike',
+            price=22.99,
+            stock=22   
+        )
+
+    with pytest.raises(ValueError):
+        product = Product(
+            name='Camisa Mike',
+            slug='cão',
+            price=22.99,
+            stock=22
+        )
+
+def test_product_schema_invalid_price():
+    with pytest.raises(ValueError):
+        product = Product(
+            name='Camisa Mike',
+            slug='camisa-mike',
+            price=0,
+            stock=22   
+        )
