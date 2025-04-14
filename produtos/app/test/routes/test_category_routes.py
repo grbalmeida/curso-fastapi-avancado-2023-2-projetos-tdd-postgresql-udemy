b454798d32f500a1bdb14625365bc53d520ba5d0
grbalmeida <g.r.almeida@live.com>
2025-04-14 12:55:48 -0300

    Ajustando testes

--- produtos/app/test/routes/test_category_routes.py
+++ produtos/app/test/routes/test_category_routes.py
@@ -4,6 +4,10 @@ from app.db.models import Category as CategoryModel
 from app.main import app
 
 client = TestClient(app)
+headers = {
+    'Authorization': 'Bearer token'
+}
+client.headers = headers
 
 def test_add_category_route(db_session):
     body = {
