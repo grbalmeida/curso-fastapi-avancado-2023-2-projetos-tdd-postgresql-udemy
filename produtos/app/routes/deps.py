7e7cbf794118b0b697956237471943f37856d3dc
grbalmeida <g.r.almeida@live.com>
2025-04-14 12:51:16 -0300

    Rotas - verificação de token nas rotas

--- produtos/app/routes/deps.py
+++ produtos/app/routes/deps.py
@@ -1,4 +1,13 @@
+from fastapi import Depends
+from sqlalchemy.orm import Session as OrmSession
+from fastapi.security import OAuth2PasswordBearer
+from decouple import config
 from app.db.connection import Session
+from app.use_cases.user import UserUseCases
+
+oauth_scheme = OAuth2PasswordBearer(tokenUrl='/user/login')
+
+TEST_MODE = config('TEST_MODE', default=False, cast=bool)
 
 def get_db_session():
     try:
@@ -6,3 +15,13 @@ def get_db_session():
         yield session
     finally:
         session.close()
+
+def auth(
+    db_session: OrmSession = Depends(get_db_session),
+    token = Depends(oauth_scheme)
+):
+    if TEST_MODE:
+        return
+
+    uc = UserUseCases(db_session=db_session)
+    uc.verify_token(token=token)
