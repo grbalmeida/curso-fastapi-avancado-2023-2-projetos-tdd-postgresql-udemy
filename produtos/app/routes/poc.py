84cca83b65ab1ac2341b6805e7360181b2c09733
grbalmeida <g.r.almeida@live.com>
2025-09-26 23:37:05 -0300

    Paginação com limit offset

--- produtos/app/routes/poc.py
+++ produtos/app/routes/poc.py
@@ -3,11 +3,12 @@ from sqlalchemy.orm import Session
 from app.routes.deps import get_db_session
 from app.schemas.category import CategoryOutput
 from app.db.models import Category as CategoryModel
-from fastapi_pagination import add_pagination, paginate, Page
+from fastapi_pagination import add_pagination, paginate, Page, LimitOffsetPage
 
 router = APIRouter(prefix='/poc', tags=['POC'])
 
 @router.get('/list', response_model=Page[CategoryOutput])
+@router.get('/list/limit-offset', response_model=LimitOffsetPage[CategoryOutput])
 def list_categories():
     categories = [
         CategoryOutput(name=f'category {n}', slug=f'category-{n}', id=n)
