4fd42ed508830994e8e0733f156ff3f0a4dd0f10
grbalmeida <g.r.almeida@live.com>
2025-09-26 23:53:44 -0300

    Paginação com SQLAlchemy

--- produtos/app/routes/poc.py
+++ produtos/app/routes/poc.py
@@ -4,6 +4,7 @@ from app.routes.deps import get_db_session
 from app.schemas.category import CategoryOutput
 from app.db.models import Category as CategoryModel
 from fastapi_pagination import add_pagination, paginate, Page, LimitOffsetPage
+from fastapi_pagination.ext.sqlalchemy import paginate as sqlalchemy_paginate
 
 router = APIRouter(prefix='/poc', tags=['POC'])
 
@@ -16,5 +17,11 @@ def list_categories():
     ]
 
     return paginate(categories)
+
+@router.get('/list/sqlalchemy', response_model=Page[CategoryOutput])
+@router.get('/list/limit-offset/sqlalchemy', response_model=LimitOffsetPage[CategoryOutput])
+def list_categories_sqlalchemy(db_session = Depends(get_db_session)):
+    categories = db_session.query(CategoryModel)
+    return sqlalchemy_paginate(categories)
 
 add_pagination(router)
